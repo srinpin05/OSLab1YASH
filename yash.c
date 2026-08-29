32f3da4c4ca5345ebeec93ac096d184e4d640c3d
srinpin05 <sspdec95@gmail.com>
2026-08-29 13:14:30 -0500

    more helper functions

--- yash.c
+++ yash.c
@@ -11,6 +11,9 @@
 
 //print array function
 
+int bg_pids[200] = {-1};
+int current_bg_amount = 0;
+
 void printarray(char** arr){
     for(int i = 0; arr[i] != NULL; i++){
         if (arr[i] == NULL){
@@ -25,12 +28,17 @@ typedef struct{
     char* inFilename;
     char* outFilename;
     char* errFilename;
+    
+    bool isBG;
     bool used_stdin;
     bool used_stdout;
     bool used_stderr;
+    bool piping; 
 
 } command;
 
+
+//processes a regular command (with file redirection) and returns a command*
 command* processCommand(char* input){
     const char a[] = " ";
     char* innerPtr = NULL;
@@ -42,9 +50,15 @@ command* processCommand(char* input){
     newcommand->used_stdin = true;
     newcommand->used_stdout = true;
     newcommand->used_stderr = true;
-
+    newcommand->isBG = false;
     newcommand->args = args;
+
+
     bool redirection = false;
+
+    //variable used to check if command is a background job
+    char* temp;
+
     while (tokens!=NULL){
         
         if(strcmp(tokens, "<")==0){
@@ -83,9 +97,114 @@ command* processCommand(char* input){
             args[c] = tokens;
             c+=1;
         }
+        temp = tokens;
         tokens = strtok_r(NULL, a, &innerPtr);
     }
+    if(strcmp(temp, "&")==0){
+        newcommand->isBG=true;
+    }
+
+
     return newcommand;
+}
+
+
+//execute a regular non piping command
+void executeRegularCommand(command* cmd){
+    if (cmd)
+
+}
+
+
+//checks if a command has piping or not given input string
+bool isPiping(char* input){
+    command** scmd = processPiping(input);
+
+    int c = 0;
+    while(scmd[c] != NULL){
+        c+=1;
+    }
+    if (c==1) {return false;}
+    else {return true;}
+}
+//processing/parses the piping command given input string
+command** processPiping(char* input){
+    char* outerPtr = NULL;
+    char* outertoken = strtok_r(input, "|", &outerPtr);
+    command** subcmds = calloc(sizeof(command*),200);
+    int c = 0;
+
+    //piping
+    while(outertoken!=NULL){
+        subcmds[c] = processCommand(outertoken);
+        char* outertoken = strtok_r(NULL, "|", &outerPtr);
+        c++;
+    }
+    return subcmds;
+}
+//executes the piping command given array of sub commands. 
+void executePiping(command** cmds){
+    int prev_fd = -1;
+        for (int j = 0; cmds[j+1] != NULL; j++){
+            int pipefd[2];
+            if (cmds[j+1] != NULL){
+                pipe(pipefd);
+            }
+            int cpid1 = fork();
+            if (cpid1 == 0){ //child
+                if (prev_fd != -1){//read end of previous child
+                    dup2(prev_fd, STDIN_FILENO);
+                    close(prev_fd);
+                }
+                if (!cmds[j]->used_stderr) {
+                    int efd = open(cmds[j]->errFilename, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+                    dup2(efd, STDERR_FILENO);
+                }
+                if (!cmds[j]->used_stdout) {
+                    int ofd = open(cmds[j]->outFilename, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+                    dup2(ofd, STDOUT_FILENO);
+                } 
+                if (!cmds[j]->used_stdin) {
+                    int ifd = open(cmds[j]->inFilename, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+                    dup2(ifd, STDIN_FILENO);
+                }
+                close(pipefd[0]); //close the unused read end
+                dup2(pipefd[1], STDOUT_FILENO);
+                execvp(cmds[j]->args[0], cmds[j]->args);
+                exit(1);
+            }
+            prev_fd = pipefd[1];
+        }
+}
+
+//starts the background job
+
+void startBackgroundJob(char* inputcmd){
+    command* bg = processCommand(inputcmd);
+    if (isPiping(inputcmd)){
+        int pid = fork();
+        if (pid == 0){
+            executePiping(processPiping(inputcmd));
+            exit(1);
+        }
+        bg_pids[current_bg_amount] = pid;
+        current_bg_amount += 1;
+
+    } else {
+        int pid = fork();
+        if (pid == 0){
+            executeRegularCommand(bg);
+            exit(1);
+        }
+        bg_pids[current_bg_amount] = pid;
+        current_bg_amount += 1;
+    }
+
+}
+
+//ends the background job
+void endBackgroundJob(){
+
 }
 
 int main(){
@@ -95,6 +214,7 @@ int main(){
     char* outerPtr = NULL;
 
     while (input){
+
         for (int i = 0; i<strlen(input);i++){
             if (input[i] == '\n'){
                 input[i] = '\0';
@@ -103,13 +223,15 @@ int main(){
 
         
 
-        char* pipetoken = strtok_r(input, "|", &outerPtr);
+        char* outertoken = strtok_r(input, "|", &outerPtr);
         command** cmds = calloc(sizeof(command*),200);
         int c = 0;
 
-
-        while(pipetoken!=NULL){
-            cmds[c] = processCommand(pipetoken);
+        //piping
+        while(outertoken!=NULL){
+            cmds[c] = processCommand(outertoken);
+            char* outertoken = strtok_r(NULL, "|", &outerPtr);
+            c++;
         }
         int prev_fd = -1;
         for (int j = 0; cmds[j+1] != NULL; j++){
@@ -142,6 +264,9 @@ int main(){
             }
             prev_fd = pipefd[1];
         }
+
+
+
     }
 
 
